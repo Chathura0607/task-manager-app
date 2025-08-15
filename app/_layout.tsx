a948e0d533c463005d8f637cd26869e8b7280572
chathura <chathuhiru45@gmail.com>
2025-08-15 15:21:47 +0530

    update index and layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,14 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { Slot, Stack } from 'expo-router'
 import "./../global.css"
+import { AuthProvider } from '@/context/AuthContext'
 
 const RootLayout = () => {
-  return <Slot />;
+  return (
+  <AuthProvider>
+    <Slot />
+  </AuthProvider>
+  );
 }
 
 export default RootLayout
--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,29 @@
-import { View, Text, Pressable } from 'react-native'
-import React from 'react'
+import { View, Text, Pressable, ActivityIndicator } from 'react-native'
+import React, { useEffect } from 'react'
 import { useRouter } from 'expo-router';
+import { useAuth } from '@/context/AuthContext';
 
 //rnfe
 const Index = () => {
     const router = useRouter()
-    
-    return (
-      <View className="flex-1 w-full justify-center align-items-center">
-        <Pressable
-          className="bg-blue-500 p-4 rounded"
-          onPress={() => router.push("/login")}
-        >
-          <Text className="text-4xl">Press Me</Text>
-        </Pressable>
-      </View>
-    );
+    const {user, loading} = useAuth()
+    console.log("User Data: ",user)
+
+    useEffect(() => {
+        if (!loading) {
+            if (user) {
+                router.push('/home')
+            } else {
+                router.push('/login')
+            }
+        }
+    }, [user, loading])
+
+    return loading ? (
+        <view className='flex-1 w-full justify-center items-center'>
+            <ActivityIndicator size="large"/>
+            </view>
+    ) : null;
 }
 
 export default Index
